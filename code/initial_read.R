c3bda5ba8349e4531accefba73021dd0daae55ae
trianglegirl <r.davies3@lancaster.ac.uk>
2017-02-15 17:58:09 +0000

    Initial commit!

---
+++ code/initial_read.R
@@ -0,0 +1,6 @@
+#Read in the data
+
+#Quite slow to load in. Maybe start with a smaller sample and save that as a data set?
+x = read.csv("data/sample1.csv", header = T, stringsAsFactors = F)
+
+#cor timeseries
